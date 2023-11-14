20cb985143401ce71b1823e511d2f4780400775f
Max Romagnoli <maxxromagnoli@gmail.com>
2023-11-14 11:39:29 +0000

    Refactor app and directory to allow fe integration.

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS  # TODO: comment out for deployment
+from api.HelloApiHandler import *
 
-app = Flask(__name__) 
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')  # add ref to frontend subdirectory
+CORS(app)  # TODO: comment out for deployment
+api = Api(app)
 
 
-# put any config we need here / or can do in separate file
+@app.route('/', defaults={'path': ''})
+def serve(path):
+    return send_from_directory(app.static_folder, 'index.html')  # retrieves react index from fe folder
 
 
-# put routes below
+api.add_resource(HelloApiHandler, '/flask/hello')
