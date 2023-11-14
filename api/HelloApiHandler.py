44461f536d37aefbe50856765f5481f4dfee5bd0
Max Romagnoli <maxxromagnoli@gmail.com>
2023-11-14 12:02:35 +0000

    Change HelloApiHandler.py to reflect axios call in fe

--- api/HelloApiHandler.py
+++ api/HelloApiHandler.py
@@ -4,5 +4,6 @@ from flask_restful import Api, Resource, reqparse
 class HelloApiHandler(Resource):
     def get(self):
         return {
+            'response': "SUCCESS",
             'message': "Hello World!"
         }
