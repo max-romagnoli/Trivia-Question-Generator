9c8002aa2932b149b58c6b606ee88a1fb6b2aa9d
Rehaan Viswanathan <viswanar@tcd.ie>
2023-11-15 21:43:18 +0000

    Replace HelloApiHandler.py

--- api/HelloApiHandler.py
+++ api/HelloApiHandler.py
@@ -1,5 +1,6 @@
+# HelloApiHandler.py
 from flask_restful import Api, Resource, reqparse
-
+from flask import jsonify, session
 
 class HelloApiHandler(Resource):
     def get(self):
@@ -7,3 +8,21 @@ class HelloApiHandler(Resource):
             'response': "SUCCESS",
             'message': "Hello World!"
         }
+# LoginApiHandler.py
+class LoginApiHandler(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('Name', type=str, required=True, help='Name cannot be blank')
+        args = parser.parse_args()
+
+        name = args['Name']
+
+        # Store the user's name in session
+        session['user_name'] = name
+        
+        response = {
+            'message': 'Login Succesfull',
+            'user_name':name
+        }
+
+        return jsonify(response), 201
