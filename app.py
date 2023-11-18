f692fd30d1f61627b97417c8f2df227738ccaa4f
Rehaan Viswanathan <viswanar@tcd.ie>
2023-11-18 02:27:39 +0000

    login route implementation

--- app.py
+++ app.py
@@ -1,32 +1,45 @@
-# app.py
-#import os
-from flask import Flask, send_from_directory, session, jsonify
-from flask_restful import Api, Resource, reqparse
+import os
+from flask import Flask, jsonify, request, session, send_from_directory
+from flask_restful import Api
 from flask_cors import CORS
 from flask_session import Session
-from api.HelloApiHandler import *
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app)
+sess = Session()
 api = Api(app)
 
-# Set the secret key
-#app.config['SECRET_KEY'] = os.urandom(24)
-
-Session(app)
-
-
-
-api.add_resource(HelloApiHandler, '/flask/hello')
-api.add_resource(LoginApiHandler, '/flask/login')
 
 @app.route('/', defaults={'path': ''})
 def serve(path):
     return send_from_directory(app.static_folder, 'index.html')
 
-#@app.route('/test')
-#def test():
-   # return jsonify({'message': 'Test endpoint'})
+# Route for handling the login page logic
+@app.route('/login', methods=['POST'])
+def login():
+    if request.is_json:
+        data = request.get_json()
+        name = data.get('Name', '')
+        
+        # Store the name in the session
+        session['Name'] = name
+
+        response_data = {"message": "Login successful", "Name": name}
+        return jsonify(response_data)
+    else:
+        return jsonify({"error": "Invalid JSON in the request"}), 400
+    
+@app.route('/get_user', methods=['GET'])
+def get_user():
+    # Retrieve the name from the session
+    name = session.get('Name', '')
+    return jsonify({"Name": name})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.secret_key = 'key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    sess.init_app(app)
+
+    app.debug = True
+    app.run()
