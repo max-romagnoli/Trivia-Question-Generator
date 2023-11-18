88f36ddd996293116562156ab5e01b272c4a39bf
Rehaan Viswanathan <viswanar@tcd.ie>
2023-11-18 02:30:11 +0000

    Update file HelloApiHandler.py

--- api/HelloApiHandler.py
+++ api/HelloApiHandler.py
@@ -9,20 +9,20 @@ class HelloApiHandler(Resource):
             'message': "Hello World!"
         }
 # LoginApiHandler.py
-class LoginApiHandler(Resource):
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('Name', type=str, required=True, help='Name cannot be blank')
-        args = parser.parse_args()
+# class LoginApiHandler(Resource):
+#     def post(self):
+#         parser = reqparse.RequestParser()
+#         parser.add_argument('Name', type=str, required=True, help='Name cannot be blank')
+#         args = parser.parse_args()
 
-        name = args['Name']
+#         name = args['Name']
 
-        # Store the user's name in session
-        session['user_name'] = name
+#         # Store the user's name in session
+#         session['user_name'] = name
         
-        response = {
-            'message': 'Login Succesfull',
-            'user_name':name
-        }
+#         response = {
+#             'message': 'Login Succesfull',
+#             'user_name':name
+#         }
 
-        return jsonify(response), 201
+#         return jsonify(response), 201
