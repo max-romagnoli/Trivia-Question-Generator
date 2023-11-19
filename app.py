755231df2c1c2ebe3c593750811a7870b57466a7
Max Romagnoli <maxxromagnoli@gmail.com>
2023-11-19 12:44:34 +0000

    Add build files for Heroku

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
-from flask_cors import CORS  # Comment out for deployment
+# from flask_cors import CORS  # Comment out for deployment
 from sys import platform
 if platform == "darwin":
     from .api.HelloApiHandler import HelloApiHandler
@@ -10,7 +10,7 @@ else:
     from api.TriviaApiHandler import TriviaApiHandler
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')  # add ref to frontend subdirectory
-CORS(app)  # Comment out for deployment
+# CORS(app)  # Comment out for deployment
 api = Api(app)
 
 
