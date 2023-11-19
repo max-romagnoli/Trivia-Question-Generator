3f94ba9d3f4b4e2db5bcca4cf48613d625e0e7b2
Max Romagnoli <maxxromagnoli@gmail.com>
2023-11-19 11:36:04 +0000

    Add build files to test deployment

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
-from flask_cors import CORS  # Comment out for deployment
+# from flask_cors import CORS  # Comment out for deployment
 from .api.HelloApiHandler import HelloApiHandler
 from .api.TriviaApiHandler import TriviaApiHandler
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')  # add ref to frontend subdirectory
-CORS(app)  # Comment out for deployment
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app_data.db'
+# CORS(app)  # Comment out for deployment
 api = Api(app)
 
 
