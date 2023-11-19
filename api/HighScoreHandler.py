4b022d5917132fc41860c994de5266ede4c82a36
oisin <gavrila@tcd.ie>
2023-11-19 15:25:53 +0000

    added global var s.t. H.S doesnt change back

--- api/HighScoreHandler.py
+++ api/HighScoreHandler.py
@@ -2,19 +2,23 @@ from flask_restful import Api, Resource, reqparse
 from flask import session, jsonify
 import requests
 
+
+highscore_value = 40
+
 # THIS IS A SAMPLE GET FUNCTION FOR GETTING HIGHSCORES
 # I NEEDED SOMETHING TO TEST MY POST FUNCTION ON
 class highScoresHandler(Resource):
      def get(self):
+          global highscore_value
           return { 
             'response': "SUCCESS",
-            'highscore': 40
+            'highscore': highscore_value
           }
     
 class highScoreApiHandler(Resource):
 
     def post(self):
-       
+        global highscore_value
         url = 'http://127.0.0.1:5000/highscores'
       
         response = requests.get(url)
@@ -26,17 +30,13 @@ class highScoreApiHandler(Resource):
                # I will definitely have to make the highscore a parameter instead of just 
                # in the function
                 new_highscore = 100
-                
+                highscore_value = new_highscore                
 
                 # Make a POST request to update the data
                 if(new_highscore >= json_data['highscore']):
-                     json_data['highscore'] = new_highscore 
-
-
-                
-                return {
-                     'response': "SUCCESS",
-                    'highscore': new_highscore
-                } 
+                    return {
+                            'response': "SUCCESS",
+                        'highscore': highscore_value
+                    } 
         else:
             return {"message": "Failed to fetch data from the endpoint."}, 500
