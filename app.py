6258101cb3b6f386d866436823e535ff895bce9b
Rehaan Viswanathan <viswanar@tcd.ie>
2023-11-19 06:51:34 +0000

    Replace app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+# main file
+
 import os
-from flask import Flask, jsonify, request, session, send_from_directory
+from flask import Flask, send_from_directory
 from flask_restful import Api
 from flask_cors import CORS
 from flask_session import Session
 from tempfile import mkdtemp
+from api.LoginApiHandler import LoginResource, GetUserResource
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app)
@@ -14,32 +17,13 @@ app.config["SESSION_FILE_DIR"] = mkdtemp()
 Session(app)
 api = Api(app)
 
-
+# Add the resources to the API for LoginApiHandler
+api.add_resource(LoginResource, '/login')
+api.add_resource(GetUserResource, '/get_user')
 
 @app.route('/', defaults={'path': ''})
 def serve(path):
     return send_from_directory(app.static_folder, 'index.html')
-
-# Route for handling the login page logic
-@app.route('/login', methods=['POST'])
-def login():
-    if request.is_json:
-        data = request.get_json()
-        name = data.get('Name', '')
-        
-        # Store the name in the session
-        session['Name'] = name
-
-        response_data = {"message": "Login successful", "Name": name}
-        return jsonify(response_data)
-    else:
-        return jsonify({"error": "Invalid JSON in the request"}), 400
-    
-@app.route('/get_user', methods=['GET'])
-def get_user():
-    # Retrieve the name from the session
-    name = session.get('Name', '')
-    return jsonify({"Name": name})
 
 if __name__ == '__main__':
     app.run()
