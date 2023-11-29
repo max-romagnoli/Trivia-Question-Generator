230206abcb5d277f806bdbd1e75e5c54916b6136
Rehaan Viswanathan <viswanar@tcd.ie>
2023-11-29 13:14:37 +0000

    Replace models.py

--- models.py
+++ models.py
@@ -19,12 +19,15 @@ class Score(db.Model):
     def __repr__(self):
         return '<id %r, username %r, score %r' % (self.id, self.username, self.score)
     
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(50), nullable=False)
+class User(db.Model):
 
-#     def __init__(self, username):
-#         self.username = username
+    username = db.Column(db.String(50), primary_key=True, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-#     def __repr__(self):
-#         return '<id %r, username %r>' % (self.id, self.username)
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return '<username %r' % (self.username)
