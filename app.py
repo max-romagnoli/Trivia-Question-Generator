61c4675cd0e54e8a4128e835215d2ea0762048e7
Max Romagnoli <maxxromagnoli@gmail.com>
2023-11-30 00:42:34 +0000

    Change route name to /register to better reflect functionality.

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS  # Comment out for deployment
 from flask_sqlalchemy import SQLAlchemy
-from flask_session import Session
-from tempfile import mkdtemp
 import os
 try:
     from .api.HelloApiHandler import HelloApiHandler
@@ -20,23 +18,17 @@ except ImportError:
 
 # initialise Flask app
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')  # add ref to frontend subdirectory
-app.config['SESSION_TYPE'] = 'filesystem'
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-Session(app)
 api = Api(app)
 CORS(app)  # Comment out for deployment
 
 # initialise database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app_data.db'
 db = SQLAlchemy(app)
-# db.drop_all()
-# db.create_all() 
 if not os.path.exists('app_data.db'):
     with app.app_context():
         try:
             from .models import *
-        except:
+        except ImportError:
             from models import *
         db.create_all()
 
@@ -50,7 +42,6 @@ def serve(path):
 api.add_resource(TriviaApiHandler, '/triviaquestion')
 api.add_resource(HelloApiHandler, '/flask/hello')
 api.add_resource(ScoresApiHandler, '/scores')
-api.add_resource(RegisterApiHandler, '/login')
-#api.add_resource(LoginApiHandler, '/get_user')
+api.add_resource(RegisterApiHandler, '/register')
 
 
